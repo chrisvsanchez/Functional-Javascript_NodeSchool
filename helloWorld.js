d78ee1db3cb75e86168c0bbe055109c5165f40eb
chrisvsanchez <chrisvsanchez@gmail.com>
2020-02-05 19:46:41 -0500

    added first three problems in functional-javascript. only two solutions

---
+++ helloWorld.js
@@ -0,0 +1,5 @@
+function upperCaser(input){
+    return input.toUpperCase();
+}
+let input = 'hello world';
+module.exports = upperCaser;
---
+++ higherOrderFunction.js
@@ -0,0 +1,10 @@
+
+    function repeat(operation, num) {
+        for( let i = 0; i <= num; i++){
+            console.log(operation);        }
+      }
+      
+      // Do not remove the line below
+      module.exports = repeat
+  
+  
